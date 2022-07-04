685aa6e31b9bc74ec5a2900b4a481279e2f45aaa
ZEROPOINT <wegj1@hotmail.com>
2022-07-04 15:38:11 -0600

    your mom

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>  
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <title> Among Us Loading Screen Garrys Mod </title>
+
+        <?php print "Your IP address is ".$_SERVER['REMOTE_ADDR'];?> <!-- This is a test for my network connection -->
+    </head>
+
+
+</html>
+
