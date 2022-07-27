52db49d9cba1055c7f08f4cb75985d3c94eeb612
ZEROPOINT <wegj1@hotmail.com>
2022-07-27 04:24:32 -0600

    I felt lazy

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
-<html>  
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        <title> Among Us Loading Screen Garrys Mod </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <?php print "Your IP address is ".$_SERVER['REMOTE_ADDR'];?> <!-- This is a test for my network connection -->
-    </head>
+    <title>Among Us Royale - 128 Players</title>
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+<body>
+    <div id="title">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
 
+        <p>Are you the Imposter from Among Us?</p>
+        <img src="./assets/img/red.gif" alt="Among Us red character">
+    </div>
+    <audio src="./assets/sfx/theme.ogg" autoplay loop></audio>
+</body>
 </html>
-
